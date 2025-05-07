e802756b7426257908c49e9650e93fd8cf563832
Reto Galante <galante@puzzle.ch>
2025-05-07 14:23:13 +0200

    Introduce generation failure

--- usage/interface/main.go
+++ usage/interface/main.go
@@ -9,8 +9,14 @@ type MyModule struct{}
 type Fooer interface {
 	DaggerObject
 	Foo(ctx context.Context, bar int) (string, error)
+	// Generation fails if return type is not a core type
+	HasBar() bool
 }
 
 func (m *MyModule) Foo(ctx context.Context, fooer Fooer) (string, error) {
 	return fooer.Foo(ctx, 42)
+}
+
+func (m *MyModule) HasBar(fooer Fooer) bool {
+    return fooer.HasBar()
 }
