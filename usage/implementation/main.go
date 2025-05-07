10c974d9aed4177c21eb62c98593efe5b67c3b15
Reto Galante <galante@puzzle.ch>
2025-05-07 14:27:18 +0200

    Add implementation of HasBar

--- usage/implementation/main.go
+++ usage/implementation/main.go
@@ -10,3 +10,7 @@ type Example struct{}
 func (m *Example) Foo(ctx context.Context, bar int) (string, error) {
 	return fmt.Sprintf("number is: %d", bar), nil
 }
+
+func (m *Example) HasBar() bool {
+	return false
+}
